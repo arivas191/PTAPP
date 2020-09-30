0f9cc6116e07134cd8eea9de47f5c7f3e709e307
AndreaMariaM <matellian.a@husky.neu.edu>
2020-09-30 19:24:55 -0400

    ignore duplicates of exercise recs, store images, cap number of conditions

--- seeds.py
+++ seeds.py
@@ -9,32 +9,37 @@ def seeds():
         Exercise(
             id=1,
             title="Seated Knee Extension",
-            description="This exercise is complete by sitting upright, bending your affected knee as far back as possible. Then holding and straightening your affected knee as far as possible and holding again, then relaxing. This motion helps strengthen your thigh muscles and aids in regaining range of motion in your knee.",
-            body_part="Knee"
+            description='This exercise is complete by sitting upright, bending your affected knee as far back as possible. Then holding and straightening your affected knee as far as possible and holding again, then relaxing. This motion helps strengthen your thigh muscles and aids in regaining range of motion in your knee.',
+            body_part="Knee",
+            image_name="SeatedKneeExtension.png"
         ),
         Exercise(
             id=2,
             title="Wrist Flexion",
-            description="This exercise is completed by placing the resistance band under your feet and pulling up and down with your hand using the wrist. This motion strengthens the muscles in the wrist, which in turn helps reduce hand tremors which can occur as a result of a stroke or Parkinson’s.",
-            body_part="Arm"
+            description='This exercise is completed by placing the resistance band under your feet and pulling up and down with your hand using the wrist. This motion strengthens the muscles in the wrist, which in turn helps reduce hand tremors which can occur as a result of a stroke or Parkinson’s.',
+            body_part="Arm",
+            image_name="WristFlexion.png"
         ),
         Exercise(
             id=3,
             title="Shoulder External Rotation",
-            description="This exercise is completed by keeping the elbow fixed and externally rotating at the shoulder to raise the band up towards the ceiling as without lifting the elbow. It strengthens your infraspinatus muscle to Improve overhead lifting and reaching",
-            body_part="Shoulder"
+            description='This exercise is completed by keeping the elbow fixed and externally rotating at the shoulder to raise the band up towards the ceiling as without lifting the elbow. It strengthens your infraspinatus muscle to Improve overhead lifting and reaching',
+            body_part="Shoulder",
+            image_name="ShoulderExternalRotation.png"
         ),
         Exercise(
             id=4,
             title="Side Hip Abduction",
-            description="This exercise is completed by laying down on the floor on your side with your head on your arm and legs bent at the knee. Keeping your feet together, lift the top leg up so that your knees are separated and then lower your leg back down slowly. This motion strengthens the hip muscles and relieves patellofemoral pain syndrome -- aka \“Runner’s Knee\”.",
-            body_part="Hips"
+            description='This exercise is completed by laying down on the floor on your side with your head on your arm and legs bent at the knee. Keeping your feet together, lift the top leg up so that your knees are separated and then lower your leg back down slowly. This motion strengthens the hip muscles and relieves patellofemoral pain syndrome -- aka "Runner’s Knee”.',
+            body_part="Hips",
+            image_name="SideHipAbduction.png"
         ),
         Exercise(
             id=5,
             title="Way Ankle Exercises",
-            description="\“A) Dorsiflexion: The pull or resistance should be pulling the foot down, so that you have to pull your foot/toes up towards you. (You may need someone to hold the band for you) -You can also anchor the other end of the tubing by tying a knot in the tubing, placing it between a door and frame then closing the door.  B) Plantarflexion: Hold band in hand so that the resistance is pulling the foot up towards you. Push foot down as if to push a gas pedal.  C) Inversion: Cross legs so the \“injured\” foot is under the \“uninjured\” leg, loop the band around the \“uninjured\” foot, holding the end of the band tight in hands. The resistance should want to pull the \“injured\” foot towards the other foot. Push the foot/ankle away from the other foot. D) Eversion: With both legs straight out about shoulder width apart, wrap the band around the ball of the \“injured\” foot then loop the other foot and hold the band. The resistance should pull the foot in towards the other foot. You will want to push the \“injured\” foot out away from the other foot\”",
-            body_part="Ankle"
+            description='“A) Dorsiflexion: The pull or resistance should be pulling the foot down, so that you have to pull your foot/toes up towards you. (You may need someone to hold the band for you) -You can also anchor the other end of the tubing by tying a knot in the tubing, placing it between a door and frame then closing the door.  B) Plantarflexion: Hold band in hand so that the resistance is pulling the foot up towards you. Push foot down as if to push a gas pedal.  C) Inversion: Cross legs so the "injured” foot is under the "uninjured” leg, loop the band around the "uninjured” foot, holding the end of the band tight in hands. The resistance should want to pull the "injured” foot towards the other foot. Push the foot/ankle away from the other foot. D) Eversion: With both legs straight out about shoulder width apart, wrap the band around the ball of the "injured” foot then loop the other foot and hold the band. The resistance should pull the foot in towards the other foot. You will want to push the "injured” foot out away from the other foot”',
+            body_part="Ankle",
+            image_name="WayAnkleExercises.png"
         )
     ]
 
