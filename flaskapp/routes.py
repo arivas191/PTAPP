c778d9a34ff5248a60917a382025c36119096802
Augusto Rivas <rivasaugusto10@gmail.com>
2020-11-23 20:31:44 -0500

    Csv file now gets cleared before starting a new workout

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -132,6 +132,10 @@ def movement(exercise):
         movement = Movement(exercise_id=exercise, user_id=current_user.get_id())
         db.session.add(movement)
         db.session.commit()
+
+        #Clear the csv file for a new exercise to be added
+        open('flaskapp/static/user_data.csv', 'w').close()
+
     return render_template('movement.html', movement=movement)
 # short-term, "start", create the movement, spinny graphic, stop button,
 # long-term user clicks "Start" -> create movement in db, collect the user data, analyze with the calculate api show user a spinny bar "working out..."  add a stop button. When stop is clicked calculate wraps up, updates the movement object, creates a feedback entry and calls feeedback api
@@ -163,27 +167,24 @@ def feedback(movement):
             csv_reader = csv.reader(csv_file, delimiter=',')
             workout = False
             reset = False
-        
+
             for row in csv_reader:
                 if len(row) == 0:
                     continue
                 if row[0] == 'reset':
                     workout = True
-                    print(workout)
                 elif workout:
                         if len(row) ==2 and not row[1] == '':
                             movements_list.append(InputMovement(datetime.datetime.fromtimestamp(int(row[0])//1000.0).time(), float(row[1])))
         movements_vector = np.array(movements_list)
 
-        print(movements_list[1].timestamp)
-        print(movements_list[1].force)
         calculations = ComputeMovementMetrics(movements_vector)
         calculations.find_max_force()
         calculations.find_repetitions()
         calculations.find_duration()
-        print(calculations.max_force)
-        print(calculations.repetitions)
-        print(calculations.duration)
+        #print(calculations.max_force)
+        #print(calculations.repetitions)
+        #print(calculations.duration)
 
         user_max_force = calculations.max_force
         user_repetitions = calculations.repetitions
