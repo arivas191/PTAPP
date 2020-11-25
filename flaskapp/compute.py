4827b43b830b096c1c247e7402fd362be6b44d53
Yodiggetydogs <nnewbury9469@gmail.com>
2020-11-25 14:52:28 -0700

    Repetition algorithm changes

--- flaskapp/compute.py
+++ flaskapp/compute.py
@@ -26,27 +26,20 @@ class ComputeMovementMetrics:
         self.max_force = max(force_measurements)
 
     def find_repetitions(self): # find the number of reps using force. this considers a positive change in force equal (with 20% margin) to at least 50% of the maximum force as a rep. 
-       # reps = 0        # baseline means force has reached the lower threshold that marks the start of a new rep. 
-        #self.force_measurements[1:] - self.force_measurments[0,end-1]
-        #lower_threshold = self.max_force*.25
-        #upper_threshold = self.max_force*.80 
-        #baseline_met = True
-        #for x in self.movements:
-         #   if baseline_met: 
-          #      if ((upper_threshold*.6) <= x.force <= (upper_threshold)):
-           #         reps += 1
-            #        baseline_met = False
-           # else:
-            #    if ((lower_threshold*.9) <= x.force <= (lower_threshold*1.1)):
-             #       baseline_met = True
-       
-        reps = 0
-        count = 0
-        for i in range(1, len(self.movements) - 1):
-            if self.movements[i].force > self.movements[i - 1].force and self.movements[i].force > 2:
-                count += 1
-            elif self.movements[i].force < self.movements[i - 1].force and count >= 4:
-                reps += 1
+        reps = 0
+
+        count = 0
+
+        for i in range(1, len(self.movements) - 1):
+
+            if self.movements[i].force > self.movements[i - 1].force and self.movements[i].force > 2:
+
+                count += 1
+
+            elif self.movements[i].force < self.movements[i - 1].force and count >= 4:
+
+                reps += 1
+
                 count = 0
         self.repetitions = reps
 
